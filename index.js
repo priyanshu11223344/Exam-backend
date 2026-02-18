da0f91f5bce564d9491df1a4df36368b36331fbe
priyanshu11223344 <priyanshupanchal2105@gmail.com>
2026-02-18 14:03:44 +0530

    done

--- index.js
+++ index.js
@@ -8,26 +8,21 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "*", // change this for production frontend URL
   })
 );
 
-// Connect DB
-connectDB();
+// Ensure DB is connected before handling request
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
 
 // Routes
-const boardRoutes = require("./routes/board.routes");
-const subjectRoutes = require("./routes/subject.routes");
-const topicRoutes = require("./routes/topic.routes");
-const paperRoutes = require("./routes/paper.routes");
-const adminUpload = require("./routes/admin.routes");
+app.use("/api/boards", require("./routes/board.routes"));
+app.use("/api/subjects", require("./routes/subject.routes"));
+app.use("/api/topics", require("./routes/topic.routes"));
+app.use("/api/papers", require("./routes/paper.routes"));
+app.use("/api/admin", require("./routes/admin.routes"));
 
-app.use("/api/boards", boardRoutes);
-app.use("/api/subjects", subjectRoutes);
-app.use("/api/topics", topicRoutes);
-app.use("/api/papers", paperRoutes);
-app.use("/api/admin", adminUpload);
-
-// 🚀 EXPORT instead of listen
 module.exports = app;
